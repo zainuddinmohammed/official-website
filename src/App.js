b2962e381e7392aebbf510f421df964b6a71e27d
Zainuddin Mohammed <zainuddin.mohammed235@gmail.com>
2024-07-30 15:26:22 -0500

    Completed basic home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import ContactPage from './ContactPage';
+import AboutPage from "./AboutPage";
+import ProjectsPage from './ProjectsPage';
+import ExperiencePage from "./ExperiencePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div>
+              <nav className="nav-bar">
+                  <ul>
+                      <li><Link to="/">Home</Link></li>
+                      <li><Link to="/about">About</Link></li>
+                      <li><Link to="/projects">Projects</Link></li>
+                      <li><Link to="/experience">Experience</Link></li>
+                      <li><Link to="/contact">Contact</Link></li>
+                  </ul>
+              </nav>
+              <Routes>
+              <Route path="/" element={<HomePage />} />
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="/projects" element={<ProjectsPage />} />
+                  <Route path="/experience" element={<ExperiencePage />} />
+                  <Route path="/contact" element={<ContactPage />} />
+              </Routes>
+          </div>
+      </Router>
   );
 }
 
