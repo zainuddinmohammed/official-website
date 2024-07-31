87c3535e273aa8aa9248ec8c332a6ffdd04251cb
Zainuddin Mohammed <zainuddin.mohammed235@gmail.com>
2024-07-30 20:57:28 -0500

    Animated home page, contact page complete

---
+++ src/AboutPage.js
@@ -0,0 +1,13 @@
+// ContactPage.js
+import React from 'react';
+
+function ContactPage() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>lorem ipsum skibidi toilet.</p>
+        </div>
+    );
+}
+
+export default ContactPage;
---
+++ src/ExperiencePage.js
@@ -0,0 +1,13 @@
+// ContactPage.js
+import React from 'react';
+
+function ExperiencePage() {
+    return (
+        <div>
+            <h1>Work Experience</h1>
+            <p>lorem ipsum skibidi toilet.</p>
+        </div>
+    );
+}
+
+export default ExperiencePage;
